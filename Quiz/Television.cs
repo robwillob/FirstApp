e5a35cff7f5cbd44b27f34c52cf3dc78cd6cff27
Robin Willoughby <robrenee27@gmail.com>
2019-02-15 11:28:40 -0600

    QuizLab Question2 Television

--- Quiz/Television.cs
+++ Quiz/Television.cs
@@ -3,20 +3,55 @@ namespace Quiz
 {
     public class Television
     {
-        public string price;
-        public string numofports;
+        public string _price;
+        public string _numofports;
+        public string _size = "65 inch";
+        public string _Mountable 
+
+           }
+
+        public string Price { get; set; } 
+
+        public string IsMountable { get; set; }
+
+        public Television() { }
+
+        public Television(string price, string numofports)
+        : this(price, numofports, " is $2000.00", "4 ports")
+        { }
 
 
-        public string Size { get; set; } = "65 inch";
-
-        public bool IsMountable { get; set; }
-
-        public void TurnOn()
+        public Television(string price, string numofports, string size, string mountable)
         {
-            Console.WriteLine("The TV is on");
+            this._size = size;
+            this._numofports = numofports;
+            this.IsMountable = mountable;
+            this.Price = price;
         }
 
-    }
+        // the following are methods
+         public void TurnOn()
+        {
+            Console.WriteLine("TV is turned on");
+        }
+
+        public void TurnOn(bool IsOn)
+        {
+            if (isOn)
+            {
+                Console.WriteLine("Open Front Door.");
+            }
+            else
+            {
+                Console.WriteLine("Open Bedroom door.");
+            }
+        }
+
+        public void CloseDoor()
+        {
+            Console.WriteLine("The door closes");
+        }
+
 
 
 
